c0a421baa7061162d72f094a49b622332cb1914d
dimkavihrov-lab <dimkavihrov@gmail.com>
2026-06-02 11:51:15 +0900

    superuser bypass limit

--- main.py
+++ main.py
@@ -230,7 +230,8 @@ def home():
     
     function generate() {
         if (isGenerating) return;
-        if (generationCount >= FREE_LIMIT && password !=='Kolqipx123098') {
+        const pass = document.getElementById('password').value;
+        if (generationCount >= FREE_LIMIT && pass !== 'Kolqipx123098') {
             alert('Лимит бесплатных генераций исчерпан. Ждите обновлений!');
             return;
         }
